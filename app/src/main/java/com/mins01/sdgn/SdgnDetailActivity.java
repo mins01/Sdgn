639d7635f30dfa13ff88d299f93ca69e4ddd60b3
mins01 <mins01@lycos.co.kr>
2016-03-01 20:05:37 +0900

    액티비티 정리 adaptor 동작 정리

--- app/src/main/java/com/mins01/sdgn/SdgnDetailActivity.java
+++ app/src/main/java/com/mins01/sdgn/SdgnDetailActivity.java
@@ -1,12 +1,15 @@
 package com.mins01.sdgn;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.volley.toolbox.NetworkImageView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -21,7 +24,7 @@ public class SdgnDetailActivity extends AppCompatActivity {
 
         try {
             row = new JSONObject(getIntent().getStringExtra("row_jsonString"));
-            this.setTitle(row.getString("unit_name"));
+            this.setTitle(row.getString("unit_name")+" - 유닛 상세");
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -69,26 +72,52 @@ public class SdgnDetailActivity extends AppCompatActivity {
     }
     private void initUI(){
         final SdgnDetailActivity thisC = this;
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        final Context context = this.getApplicationContext();
+        UnitCardHolder unitCardHolder = new UnitCardHolder();
+        View convertView = this.findViewById(R.id.activity_sdgn_detail_top);
+        unitCardHolder.setMemberVar(convertView);
+        unitCardHolder.setValues(row,convertView);
+        try{
+            //-- 상단 부분
+            NetworkImageView imageView_unit_anime_img =(NetworkImageView) this.findViewById(R.id.imageView_unit_anime_img);
+            imageView_unit_anime_img.setImageUrl(row.getString("unit_anime_img"), MySingleton.getInstance(context).getImageLoader());
+            ((TextView) this.findViewById(R.id.textView_unit_anime)).setText(row.getString("unit_anime"));
+            ((TextView) this.findViewById(R.id.textView_unit_txt)).setText(row.getString("unit_txt"));
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
 
-        //리로드 버튼
-        Button main_btn_reload = (Button) findViewById(R.id.main_btn_reload);
-        main_btn_reload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                firstLoad();
+            //-- 내용 부분
+            String tmp;
+            LinearLayout imgs,texts;
+
+            imgs = (LinearLayout)this.findViewById(R.id.group_unit_weapon_img);
+            texts = (LinearLayout)this.findViewById(R.id.group_unit_weapon);
+            for(int i=0,m=3;i<m;i++){
+                tmp = row.getString("unit_weapon"+(i+1)+"_img");
+                if(tmp!=null && tmp.length()>1){
+                    ((NetworkImageView)imgs.getChildAt(i)).setImageUrl(tmp, MySingleton.getInstance(context).getImageLoader());
+                    ((TextView)texts.getChildAt(i)).setText(row.getString("unit_weapon" + (i + 1)));
+                }else{
+                    ((NetworkImageView)imgs.getChildAt(i)).setImageResource(android.R.color.transparent);//초기화
+                    ((TextView)texts.getChildAt(i)).setText("");
+                }
             }
-        });
+             imgs = (LinearLayout)this.findViewById(R.id.group_unit_skill_img);
+             texts = (LinearLayout)this.findViewById(R.id.group_unit_skill);
+            for(int i=0,m=3;i<m;i++){
+                tmp = row.getString("unit_skill"+(i+1)+"_img");
+                if(tmp!=null && tmp.length()>1){
+                    ((NetworkImageView)imgs.getChildAt(i)).setImageUrl(tmp, MySingleton.getInstance(context).getImageLoader());
+                    ((TextView)texts.getChildAt(i)).setText( row.getString("unit_skill"+(i+1)));
+                }else{
+                    ((NetworkImageView)imgs.getChildAt(i)).setImageResource(android.R.color.transparent);//초기화
+                    ((TextView)texts.getChildAt(i)).setText("");
+                }
+            }
+
+        }catch (Exception e){
+
+        }
+
     }
     public void firstLoad(){
         initUI();
@@ -105,6 +134,7 @@ public class SdgnDetailActivity extends AppCompatActivity {
             protected void onPostExecute(SdgnDetailActivity result) {
                 //super.onPostExecute(result);
                 //result.firstLoad();
+                initUI();
             }
 
         }).execute(this);
